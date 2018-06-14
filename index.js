4a2be7bf4b15684fd020bfabaef730fa20cdfc87
Mai Diep <diepmv@vccloud.vn>
2018-06-14 21:49:45 +0700

    string

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ import Redis from 'ioredis';
 
 const redis = new Redis();
 
-redis.set('name', "Mai Van Diep");
+redis.set('name', "Mai Van Diep", "EX", 5);
 redis.get("name", (err, result) => {
   console.log(result);
-})
+});
+
+redis.incrby('address', 300);
+
+redis.get("address", (err, result) => {
+  console.log(result);
+});
+
+redis.mset("name", "Diep", "address", 36);
+redis.mget("name", "address", (err, result) => {
+  console.log(result);
+});
